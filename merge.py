deea1b6dc9a6bea06005f6884c90826024c098ac
webVerts <100699190+webVerts@users.noreply.github.com>
2023-02-01 21:23:49 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -2,11 +2,7 @@ import json
 import glob
 
 result = []
-path = "./merge_action_test/folder1/.*json"
+path = "merge_action_test/folder1/*json"
 for filename in glob.glob(path, recursive=True):
     with open(filename, 'r') as infile:
-        print("path:")
-        print(path);
-        result.append(json.load(infile))
-with open("merged_file.json", "w") as outfile:
-     json.dump(result, outfile)  
+       
