3a01685259692fd73a85cf2782084ce373021414
webVerts <100699190+webVerts@users.noreply.github.com>
2023-02-03 15:35:17 +0530

    Update merge.py

--- merge.py
+++ merge.py
@@ -7,7 +7,7 @@ for filename in glob.glob(path, recursive=True):
     with open(filename, 'r') as infile:
         print(filename);
        
-        result.extend(json.load(infile))
+        result.append(json.load(infile))
 with open("./merged_file.json", "w") as outfile:
      json.dump(result, outfile) 
      print(result)
