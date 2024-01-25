89392fc2d2fac32e50ae844965cfd06da8d34a62
webVerts <100699190+webVerts@users.noreply.github.com>
2024-01-25 15:53:35 +0530

    Create update_filters.py

---
+++ update_filters.py
@@ -0,0 +1,11 @@
+import requests
+
+# Set the URL of the script.js file in the GitHub repository
+url = "https://raw.githubusercontent.com/authifyWeb/filters/main/URL/filters.js"
+
+# Send an HTTP GET request to retrieve the file
+response = requests.get(url)
+
+# Write the retrieved data to a new file
+with open("filters.js", "w") as f:
+    f.write(response.text)
